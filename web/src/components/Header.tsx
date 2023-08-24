a6c467b5bdf20fa2a063afa4093f5b09a342cfc0
NoahELE <cxhmel@outlook.com>
2023-08-24 23:04:41 +1000

    refactor: changed the header

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, Button, Toolbar } from '@mui/material';
+import { AppBar, Box, Button, Stack, Toolbar } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
@@ -6,21 +6,23 @@ export default function Header() {
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
-        <Box sx={{ marginRight: '5rem' }}>
-          <img src="/vite.svg" alt="LOGO" />
+        <Box sx={{ flexGrow: 1 }}>
+          <img src="/react.svg" alt="LOGO" />
         </Box>
-        <Button color="inherit" onClick={() => navigate('/')}>
-          Home
-        </Button>
-        <Button color="inherit" onClick={() => navigate('/recording')}>
-          Recording Management
-        </Button>
-        <Button color="inherit" onClick={() => navigate('/public')}>
-          Public Records
-        </Button>
-        <Button color="inherit" onClick={() => navigate('/signin')}>
-          Sign in/up
-        </Button>
+        <Stack direction="row" spacing={1}>
+          <Button color="inherit" onClick={() => navigate('/')}>
+            Home
+          </Button>
+          <Button color="inherit" onClick={() => navigate('/recording')}>
+            Recording Management
+          </Button>
+          <Button color="inherit" onClick={() => navigate('/public')}>
+            Public Records
+          </Button>
+          <Button color="inherit" onClick={() => navigate('/signin')}>
+            Sign in/up
+          </Button>
+        </Stack>
       </Toolbar>
     </AppBar>
   );
