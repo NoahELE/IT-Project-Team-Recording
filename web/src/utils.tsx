a92575da61a112cb1532550d761cd9b962c52c77
Noah <cxhmel@outlook.com>
2023-08-30 15:56:49 +1000

    refactor: add close button to snackbar

--- web/src/utils.tsx
+++ web/src/utils.tsx
@@ -1,17 +1,27 @@
-import { Snackbar } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton, Snackbar } from '@mui/material';
 import { ReactElement, useCallback, useState } from 'react';
 
 export function useShowError(): [ReactElement, (error: Error) => void] {
   const [open, setOpen] = useState(false);
   const [error, setError] = useState<Error | null>(null);
+  const onClose = () => setOpen(false);
   const snackbar = (
     <Snackbar
       open={open}
       message={error?.message}
+      action={
+        <IconButton
+          size="small"
+          aria-label="close"
+          color="inherit"
+          onClick={onClose}
+        >
+          <CloseIcon fontSize="small" />
+        </IconButton>
+      }
       autoHideDuration={5000}
-      onClose={() => {
-        setOpen(false);
-      }}
+      onClose={onClose}
     />
   );
 
