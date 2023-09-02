c912d3f41d440ad023b0070384c917c5ae905090
chenhaof <chenhaof@student.unimelb.edu.au>
2023-09-02 20:23:44 +1000

    Merge with main

--- web/src/views/PublicRecordsView.tsx
+++ web/src/views/PublicRecordsView.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Stack, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 export default function PublicRecordsView() {
   return (
@@ -6,46 +6,6 @@ export default function PublicRecordsView() {
       <Typography variant="h2" component="h1">
         TO DO...
       </Typography>
-      <Stack direction="row" spacing={2}>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <Box
-              sx={{
-                backgroundColor: 'red',
-              }}
-            >
-              <Typography variant="h4">Human Audio Recording Tool</Typography>
-              <Box mt={5}>
-                <Typography variant="body1">Subtitle 1</Typography>
-              </Box>
-            </Box>
-          </Grid>
-          <Grid item xs={4}>
-            <Box
-              sx={{
-                backgroundColor: 'green',
-              }}
-            >
-              <Typography variant="h4">C-LARA Platform</Typography>
-              <Box mt={5}>
-                <Typography variant="body1">Subtitle 1</Typography>
-              </Box>
-            </Box>
-          </Grid>
-          <Grid item xs={4}>
-            <Box
-              sx={{
-                backgroundColor: 'blue',
-              }}
-            >
-              <Typography variant="h4">Title 3</Typography>
-              <Box mt={5}>
-                <Typography variant="body1">Subtitle 1</Typography>
-              </Box>
-            </Box>
-          </Grid>
-        </Grid>
-      </Stack>
     </Box>
   );
 }
--- web/src/views/RecordingView.tsx
+++ web/src/views/RecordingView.tsx
@@ -1,4 +1,11 @@
-import { Button, Container, Divider, Stack, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Stack,
+  Typography,
+} from '@mui/material';
 import { GridRowSelectionModel } from '@mui/x-data-grid';
 import { useEffect, useState } from 'react';
 import { useReactMediaRecorder } from 'react-media-recorder';
@@ -43,57 +50,61 @@ export default function RecordingView() {
     useState<GridRowSelectionModel>([]);
 
   return (
-    <Container>
-      <Stack spacing={5}>
-        <Stack direction="row" spacing={10}>
-          <Stack spacing={3}>
-            <Stack direction="row" spacing={3}>
-              <Button
-                variant="contained"
-                disabled={isRecording}
-                onClick={() => {
-                  startRecording();
-                  setRecordedTime(0.0);
-                }}
-              >
-                Start Recording
-              </Button>
-              <Button
-                variant="contained"
-                disabled={!isRecording}
-                onClick={stopRecording}
-              >
-                Stop Recording
-              </Button>
+    <Box mt={10}>
+      <Container>
+        <Stack spacing={5}>
+          <Stack direction="row" spacing={10}>
+            <Stack spacing={3}>
+              <Stack direction="row" spacing={3}>
+                <Button
+                  variant="contained"
+                  disabled={isRecording}
+                  onClick={() => {
+                    startRecording();
+                    setRecordedTime(0.0);
+                  }}
+                >
+                  Start Recording
+                </Button>
+                <Button
+                  variant="contained"
+                  disabled={!isRecording}
+                  onClick={stopRecording}
+                >
+                  Stop Recording
+                </Button>
+              </Stack>
+              <Typography variant="h5">
+                Recorded Time: {recordedTime.toFixed(1)} seconds.
+              </Typography>
             </Stack>
-            <Typography variant="h5">
-              Recorded Time: {recordedTime.toFixed(1)} seconds.
-            </Typography>
+            {mediaBlobUrl !== undefined && (
+              <audio src={mediaBlobUrl} controls />
+            )}
           </Stack>
-          {mediaBlobUrl !== undefined && <audio src={mediaBlobUrl} controls />}
+
+          <Divider variant="middle" />
+
+          <RecordingList
+            recordings={recordings}
+            rowSelectionModel={rowSelectionModel}
+            setRowSelectionModel={setRowSelectionModel}
+          />
+
+          <Stack direction="row-reverse" spacing={3}>
+            <Button
+              onClick={() => {
+                rowSelectionModel.forEach((id) => {
+                  // TODO: delete the selected recordings
+                  console.log(id);
+                });
+              }}
+            >
+              Delete Selected
+            </Button>
+          </Stack>
         </Stack>
-
-        <Divider variant="middle" />
-
-        <RecordingList
-          recordings={recordings}
-          rowSelectionModel={rowSelectionModel}
-          setRowSelectionModel={setRowSelectionModel}
-        />
-
-        <Stack direction="row-reverse" spacing={3}>
-          <Button
-            onClick={() => {
-              rowSelectionModel.forEach((id) => {
-                // TODO: delete the selected recordings
-                console.log(id);
-              });
-            }}
-          >
-            Delete Selected
-          </Button>
-        </Stack>
-      </Stack>
-    </Container>
+      </Container>
+    </Box>
   );
 }
