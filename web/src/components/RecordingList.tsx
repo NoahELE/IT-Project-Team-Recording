fa7daf05a31a8c2acb23158e4a7d46dd289fc7f5
NoahELE <cxhmel@outlook.com>
2023-09-07 20:00:17 +1000

    feat: fold text in table

--- web/src/components/RecordingList.tsx
+++ web/src/components/RecordingList.tsx
@@ -1,16 +1,127 @@
-import { Box } from '@mui/material';
+import { Box, Paper, Popper, Typography } from '@mui/material';
 import {
   DataGrid,
   GridColDef,
   GridRenderCellParams,
   GridRowSelectionModel,
 } from '@mui/x-data-grid';
+import { memo, useEffect, useRef, useState } from 'react';
 import { Recording } from '../entity';
 
-interface Props {
-  recordings: Recording[];
-  rowSelectionModel?: GridRowSelectionModel;
-  setRowSelectionModel?: (rowSelectionModel: GridRowSelectionModel) => void;
+interface GridCellExpandProps {
+  value: string;
+  width: number;
+}
+
+function isOverflown(element: Element): boolean {
+  return (
+    element.scrollHeight > element.clientHeight ||
+    element.scrollWidth > element.clientWidth
+  );
+}
+
+const GridCellExpand = memo(function GridCellExpand(
+  props: GridCellExpandProps,
+) {
+  const { width, value } = props;
+  const wrapper = useRef<HTMLDivElement | null>(null);
+  const cellDiv = useRef(null);
+  const cellValue = useRef(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [showFullCell, setShowFullCell] = useState(false);
+  const [showPopper, setShowPopper] = useState(false);
+
+  const handleMouseEnter = () => {
+    const isCurrentlyOverflown = isOverflown(cellValue.current!);
+    setShowPopper(isCurrentlyOverflown);
+    setAnchorEl(cellDiv.current);
+    setShowFullCell(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowFullCell(false);
+  };
+
+  useEffect(() => {
+    if (!showFullCell) {
+      return undefined;
+    }
+
+    function handleKeyDown(nativeEvent: KeyboardEvent) {
+      // IE11, Edge (prior to using Bink?) use 'Esc'
+      if (nativeEvent.key === 'Escape' || nativeEvent.key === 'Esc') {
+        setShowFullCell(false);
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [setShowFullCell, showFullCell]);
+
+  return (
+    <Box
+      ref={wrapper}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      sx={{
+        alignItems: 'center',
+        lineHeight: '24px',
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+        display: 'flex',
+      }}
+    >
+      <Box
+        ref={cellDiv}
+        sx={{
+          height: '100%',
+          width,
+          display: 'block',
+          position: 'absolute',
+          top: 0,
+        }}
+      />
+      <Box
+        ref={cellValue}
+        sx={{
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        }}
+      >
+        {value}
+      </Box>
+      {showPopper && (
+        <Popper
+          open={showFullCell && anchorEl !== null}
+          anchorEl={anchorEl}
+          style={{ width, marginLeft: -17 }}
+        >
+          <Paper
+            elevation={1}
+            style={{ minHeight: wrapper.current!.offsetHeight - 3 }}
+          >
+            <Typography variant="body2" style={{ padding: 8 }}>
+              {value}
+            </Typography>
+          </Paper>
+        </Popper>
+      )}
+    </Box>
+  );
+});
+
+function renderCellExpand(params: GridRenderCellParams<Recording, string>) {
+  return (
+    <GridCellExpand
+      value={params.value || ''}
+      width={params.colDef.computedWidth}
+    />
+  );
 }
 
 const columns: GridColDef[] = [
@@ -38,8 +149,19 @@ const columns: GridColDef[] = [
       );
     },
   },
-  { field: 'text', headerName: 'Text', width: 200 },
+  {
+    field: 'text',
+    headerName: 'Text',
+    width: 200,
+    renderCell: renderCellExpand,
+  },
 ];
+
+interface Props {
+  recordings: Recording[];
+  rowSelectionModel?: GridRowSelectionModel;
+  setRowSelectionModel?: (rowSelectionModel: GridRowSelectionModel) => void;
+}
 
 export default function RecordingList({
   recordings,
