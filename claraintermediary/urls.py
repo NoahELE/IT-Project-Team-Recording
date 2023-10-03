7879ea1667df1c085b7ff0eacf985b294a3c8029
Bill Nguyen <bn.billnguyen@gmail.com>
2023-10-04 01:09:00 +1100

    revising url paths for clara

--- claraintermediary/urls.py
+++ claraintermediary/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddBatchJobView, DeleteJobsWithTaskID, ClearTaskID, ChangeUserOnTaskID
+from .views import AddBatchJobView, DeleteJobsWithTaskID, ClearTaskID, ChangeUserOnTaskID, FilterUsers
 
 urlpatterns = [
-        path('clara/add-batch-job', AddBatchJobView.as_view(), name='add-batch-job'),
-        path('clara/delete-task', DeleteJobsWithTaskID.as_view(), name='delete-task'),
-        path('clara/clear-task', ClearTaskID.as_view(), name='clear-task'),
-        path('clara/change-user-task', ChangeUserOnTaskID.as_view(), name='change-user-task'),
+        path('clara/task/add', AddBatchJobView.as_view(), name='add-batch-job'),
+        path('clara/task/delete', DeleteJobsWithTaskID.as_view(), name='delete-task'),
+        path('clara/task/clear', ClearTaskID.as_view(), name='clear-task'),
+        path('clara/task/change-user', ChangeUserOnTaskID.as_view(), name='change-user-task'),
+        path('clara/users/get', FilterUsers.as_view(), name='filter-users')
 ]
