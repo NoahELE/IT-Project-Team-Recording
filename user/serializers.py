802db88d99b5e7c2531fa7a26f33fe8f920f8e71
JIANYI ZHANG <jianyi@student.unimelb.edu.au>
2023-10-16 22:55:13 +1100

    delete the unused django folder and fix some bug

--- user/serializers.py
+++ user/serializers.py
@@ -30,10 +30,10 @@ class UserSerializer(serializers.ModelSerializer):
 class EditProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserData
-        fields = ['name', 'email']
+        fields = ['username', 'email']
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
+        instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
         instance.save()
         return instance
